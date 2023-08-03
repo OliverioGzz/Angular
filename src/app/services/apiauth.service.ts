e36d36b1085df095c499cb7e79677f1d5bd7ca09
OliverioGzz <oliverio.gzz@outlook.com>
2023-08-02 19:24:51 -0600

    Se suben cambios 2

--- src/app/services/apiauth.service.ts
+++ src/app/services/apiauth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, map } from "rxjs";
 import { Response } from "../models/response";
+import { Usuario } from "../models/usuario";
 
 const httpOption = {
     headers: new HttpHeaders({
@@ -14,12 +15,28 @@ const httpOption = {
 })
 export class ApiauthService{
     url: string = 'https://localhost:7248/api/User/login';
-
+    private usuarioSubject: BehaviorSubject<Usuario>;
+    public get usuarioData(): Usuario{
+      return this.usuarioSubject.value;
+    }
     constructor(private _http: HttpClient){
-
+      this.usuarioSubject = new BehaviorSubject<Usuario>(JSON.parse(localStorage.getItem('usuario')!));
     }
 
     login(email: string, password: string): Observable<Response>{
-        return this._http.post<Response>(this.url, {email, password}, httpOption);
+        return this._http.post<Response>(this.url, {email, password}, httpOption).pipe(
+          map(res => {
+            if (res.exito === 1) {
+              const usuario: Usuario = res.data;
+              localStorage.setItem('usuario', JSON.stringify(usuario));
+              this.usuarioSubject.next(usuario);
+            }
+            return res;
+          })
+        );
+    }
+
+    logout(){
+      localStorage.removeItem(null!)
     }
 }
